f925c0cf4092835bdbe81154da89df55b6b2cdbd
Akashi169 <162897309+Akashi169@users.noreply.github.com>
2025-03-16 07:39:52 +0700

    Add new map + explosion of rocket

---
+++ globals.py
@@ -0,0 +1,8 @@
+import pygame
+game_over = False
+all_sprite = pygame.sprite.Group()
+environment_group = pygame.sprite.Group()
+enemies_group = pygame.sprite.Group()
+button_game_over = pygame.sprite.Group()
+button_upgrade = pygame.sprite.Group()
+gold_group = pygame.sprite.Group()
