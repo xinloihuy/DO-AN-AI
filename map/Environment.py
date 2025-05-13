2ffcc7130178d0c28bdb2157c679daee77f6d0f8
Akashi169 <162897309+Akashi169@users.noreply.github.com>
2025-05-14 06:44:35 +0700

    add animated wave

--- map/Environment.py
+++ map/Environment.py
@@ -21,8 +21,32 @@ class Ground(EnvironmentTiles):
     def __init__(self,x,y,image):
         super().__init__(x,y,image)
 class Water(EnvironmentTiles):
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
+class AnimatedWater(EnvironmentTiles):
     def __init__(self,x,y,image):
         super().__init__(x,y,image)
+        self.frames = [
+            pygame.image.load(f"assets/images/55.png"),
+            pygame.image.load(f"assets/images/56.png"),
+            pygame.image.load(f"assets/images/57.png"),
+            pygame.image.load(f"assets/images/58.png")
+        ]
+        self.frames = [pygame.transform.scale(frame, (tile_size, tile_size)) for frame in self.frames]
+        self.current_frame = 0
+        self.frame_count = len(self.frames)
+        self.animation_speed = 100
+        self.time_since_last_frame = 0
+        
+        self.image = self.frames[self.current_frame]
+    
+    def update(self, dt):
+        self.time_since_last_frame += dt 
+        
+        if self.time_since_last_frame >= self.animation_speed:
+            self.current_frame = (self.current_frame + 1) % self.frame_count
+            self.image = self.frames[self.current_frame]
+            self.time_since_last_frame = 0 
 class Tree(EnvironmentTiles):
     def __init__(self,x,y,image):
         super().__init__(x,y,image)
